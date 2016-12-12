5accacd042ea7e875d560937909b6d76de8fb79a
Lachlan Donald <lachlan@ljd.cc>
2016-12-12 20:56:09 +1100

    Update to use official node docker images

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 node_sass_version=3.7.0
-node_versions=( 0.10 0.12 4 5 6 )
+node_versions=( 4 6 7 )
 image_name=node-sass-alpine-builder
 
 checkout_source() {
@@ -11,33 +11,50 @@ checkout_source() {
 	[[ -d node-sass/.git ]] || git clone https://github.com/sass/node-sass --recursive
 	(
 	cd node-sass
+	rm -rf vendor/
 	git clean -f -d
 	git checkout "v$version"
 	git submodule update --init --recursive
 	)
 }
 
-build_docker_image() {
+dockerfile() {
 	local version="$1"
-	echo "Building $image_name:${version}"
-	docker build --tag "$image_name:${version}" - <<- EOF
-	FROM mhart/alpine-node:${version}
+	local file="$2"
+
+	echo "Generating $file"
+	cat <<- EOF > $file
+	FROM node:${version}-alpine
+	ARG NODE_SASS_VERSION
 	RUN apk add --no-cache python=2.7.12-r0 git-perl bash make gcc g++
 	RUN rm /bin/sh && ln -s /bin/bash /bin/sh
 	WORKDIR /node-sass
+	COPY ./node-sass/package.json /node-sass/package.json
+	RUN npm install --verbose
+	COPY ./node-sass /node-sass
 	EOF
+}
+
+build_docker_image() {
+	local version="$1"
+	dockerfile "$version" "Dockerfile.${version}"
+	echo "Building $image_name:${version}"
+	docker build --tag "$image_name:${version}" -f "Dockerfile.${version}" \
+		--build-arg NODE_SASS_VERSION=${node_sass_version} .
+	rm "Dockerfile.${version}"
 }
 
 build_node_sass() {
 	local version="$1"
+	local volumes=(
+		"$PWD/node-sass:/node-sass"
+		"$PWD/build/${version}:/build"
+	)
 	set -x
 	docker run \
-		-v "$PWD/.npm:/root/.npm" \
-		-v "$PWD/node-sass:/node-sass" \
-		-v "$PWD/build/${version}:/node-sass/vendor" \
-		-v "node_modules_${version}:/node-sass/node_modules" \
-		--rm "$image_name:${version}" \
-			bash -c "npm install --depth=1 && node scripts/build.js -f --verbose"
+		${volumes[@]/#/-v } \
+		-it --rm "$image_name:${version}" \
+			bash -c "node scripts/build.js -f --verbose && cp -a vendor/* /build"
 }
 
 ## Main
@@ -48,5 +65,4 @@ checkout_source "$node_sass_version"
 for version in "${node_versions[@]}" ; do
 	build_docker_image "$version"
 	build_node_sass "$version"
-	# docker run node-sass-binaries cat /node-sass/binaries.tar.gz > "binary-$i.tar.gz"
 done
